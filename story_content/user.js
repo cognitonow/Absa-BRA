ceecfc01ea2510fc57442a41081f77df50eea11f
cognitonow <cognito.learning.now@gmail.com>
2020-05-12 14:56:28 +0200

    sdf
    sdf

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68VMfdmkx4q":
+        Script1();
+        break;
+      case "6K3y7IHUn2D":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
+function Script2()
+{
+  SetStatus("completed")
+}
+
